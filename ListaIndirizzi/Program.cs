d6a574c2b2b5240f2a3c20dec053796decd46fa0
Pedra96 <darkraidialga@yahoo.it>
2022-12-12 15:09:49 +0100

    Add project files.

---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi
+contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: 
+vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+*/
+using ListaIndirizzi;
+using System.Linq.Expressions;
+
+List<indirizzo> ListaIndirizzi = new();
+
+try {
+    StreamReader file = File.OpenText(@"F:\Generation_Ita_Esercizi\ListaIndirizzi\ListaIndirizzi\Indirizzi\addresses.csv");
+    while (!file.EndOfStream) {
+        string riga = file.ReadLine();
+
+
+        string[] InformazioniIndirizzo = riga.Split(",");
+        for(int i = 0;i<InformazioniIndirizzo.Length;i++) {
+            if (string.IsNullOrEmpty(InformazioniIndirizzo[i])){
+                InformazioniIndirizzo[i] = "Error";
+            }
+        }
+
+        string Name = InformazioniIndirizzo[0];
+        string Surname = InformazioniIndirizzo[1];
+        string Street = InformazioniIndirizzo[2];
+        string City = InformazioniIndirizzo[3];
+        string Province = InformazioniIndirizzo[4];
+        string ZIP = InformazioniIndirizzo[5];
+        indirizzo IndirizzoEstratto = new(Name, Surname, Street, City, Province, ZIP);
+        ListaIndirizzi.Add(IndirizzoEstratto);
+    }
+    file.Close();
+}
+catch (Exception e) {
+    Console.WriteLine(e);
+}
+
+foreach(var elemento in ListaIndirizzi) {
+    Console.WriteLine(elemento);
+}
---
+++ ListaIndirizzi/indirizzo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi {
+    public class indirizzo {
+
+        private string Name;
+        private string Surname;
+        private string Street;
+        private string City;
+        private string Province;
+        private string ZIP;
+
+        public indirizzo(string Name, string Surname, string Street,string City,string Province,string ZIP) {
+            this.Name=Name;
+            this.Surname=Surname;
+            this.Street=Street;
+            this.City = City;
+            this.Province = Province;
+            this.ZIP = ZIP;
+        }
+
+        public string GetName() {
+            return Name;
+        }
+
+        public void SetName(string value) {
+            Name = value;
+        }
+
+        public string GetSurname() {
+            return Surname;
+        }
+
+        public void SetSurname(string value) {
+            SetSurname(value);
+        }
+
+        public string GetStreet() {
+            return Street;
+        }
+
+        public void SetStreet(string value) {
+            Street = value;
+        }
+        public string GetCity() {
+            return City;
+        }
+
+        public void SetCity(string value) {
+            City = value;
+        }
+        public string GetProvince() {
+            return Province;
+        }
+
+        public void SetProvince(string value) {
+            Province = value;
+        }
+        public string GetZIP() {
+            return ZIP;
+        }
+
+        public void SetZIP(string value) {
+            ZIP = value;
+        }
+
+        public override string ToString() {
+            string print = GetName() + "\t";
+            print += GetSurname() + "\t";
+            print += GetStreet() + "\t";
+            print += GetCity() + "\t";
+            print += GetProvince() + "\t";
+            print += GetZIP() + "\t";
+            return print;
+        }
+    }
+}
